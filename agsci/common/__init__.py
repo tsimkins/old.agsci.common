0137ad688e871b853cb778e6993d2b8917778438
tsimkins <trs22@psu.edu>
2015-03-17 09:48:19 -0400

    Initial commit

---
+++ agsci/common/__init__.py
@@ -0,0 +1,5 @@
+from zope.i18nmessageid import MessageFactory
+commonMessageFactory = MessageFactory('agsci.common')
+
+def initialize(context):
+    pass
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 
+    'agsci', 'common', 'version.txt')).read().strip()
+
+setup(name='agsci.common',
+    version=version,
+    description="",
+    long_description=open("README.txt").read() + "\n" +
+                     open("HISTORY.txt").read(),
+    # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+      "Framework :: Plone",
+      "Programming Language :: Python",
+      "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+    keywords='',
+    author='Tim Simkins, College of Agricultural Sciences, Penn State University',
+    author_email='trs22@psu.edu',
+    url='http://agsci.psu.edu/',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['agsci'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+      'setuptools',
+      # -*- Extra requirements: -*-
+      ],
+    entry_points="""
+      # -*- Entry points: -*-
+      """,
+    )
