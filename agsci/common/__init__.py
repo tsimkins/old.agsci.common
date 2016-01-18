5a66d59713de1f3d41c713fb11edac261aa106b0
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2016-01-18 16:29:43 -0500

    Adding helper function to ploneify ids.

--- agsci/common/__init__.py
+++ agsci/common/__init__.py
@@ -1,6 +1,14 @@
 from zope.i18nmessageid import MessageFactory
+import re
 
 AgSciMessageFactory = MessageFactory('agsci.common')
 
 def initialize(context):
     pass
+
+#Ploneify
+def ploneify(toPlone):
+    ploneString = re.sub("[^A-Za-z0-9]+", "-", toPlone).lower()
+    ploneString = re.sub("-$", "", ploneString)
+    ploneString = re.sub("^-", "", ploneString)
+    return ploneString
