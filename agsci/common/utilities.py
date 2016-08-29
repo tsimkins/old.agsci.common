b3f4aa80d8d78cf109fc487d640090d1707ba79f
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2016-08-29 09:29:40 -0400

    Moved `execute_under_special_role` method to `agsci.common.utilities` since we'll be using it in multiple places.

--- agsci/common/utilities.py
+++ agsci/common/utilities.py
@@ -1,3 +1,6 @@
+from AccessControl import getSecurityManager
+from AccessControl.SecurityManagement import newSecurityManager, setSecurityManager
+from AccessControl.User import UnrestrictedUser as BaseUnrestrictedUser
 from DateTime import DateTime
 from datetime import datetime
 import pytz
@@ -17,15 +20,15 @@ def iso_to_datetime(v):
 
 # Convert a Plone DateTime to a ISO formated string
 def toISO(v):
-            
+
     if isinstance(v, DateTime):
         try:
             tz = pytz.timezone(v.timezone())
         except pytz.UnknownTimeZoneError:
             # Because that's where we are.
-            tz = pytz.timezone(default_timezone) 
-    
-        tmp_date = datetime(v.year(), v.month(), v.day(), v.hour(), 
+            tz = pytz.timezone(default_timezone)
+
+        tmp_date = datetime(v.year(), v.month(), v.day(), v.hour(),
                             v.minute(), int(v.second()))
 
         if tmp_date.year not in [2499, 1000]:
@@ -50,7 +53,7 @@ def getContentType(i):
             else:
                 return v
     return None
-            
+
 def increaseHeadingLevel(text):
     if '<h2' in text:
         for i in reversed(range(1,6)):
@@ -59,3 +62,60 @@ def increaseHeadingLevel(text):
             text = text.replace("<%s" % from_header, "<%s" % to_header)
             text = text.replace("</%s" % from_header, "</%s" % to_header)
     return text
+
+# Copied almost verbatim from http://docs.plone.org/develop/plone/security/permissions.html
+
+class UnrestrictedUser(BaseUnrestrictedUser):
+    """Unrestricted user that still has an id.
+    """
+    def getId(self):
+        """Return the ID of the user.
+        """
+        return self.getUserName()
+
+def execute_under_special_role(portal, roles, function, *args, **kwargs):
+    """ Execute code under special role privileges.
+
+    Example how to call::
+
+        execute_under_special_role(portal, "Manager",
+            doSomeNormallyNotAllowedStuff,
+            source_folder, target_folder)
+
+
+    @param portal: Reference to ISiteRoot object whose access controls we are using
+
+    @param function: Method to be called with special privileges
+
+    @param roles: User roles for the security context when calling the privileged code; e.g. "Manager".
+
+    @param args: Passed to the function
+
+    @param kwargs: Passed to the function
+    """
+
+    sm = getSecurityManager()
+
+    try:
+        try:
+            # Clone the current user and assign a new role.
+            # Note that the username (getId()) is left in exception
+            # tracebacks in the error_log,
+            # so it is an important thing to store.
+            tmp_user = UnrestrictedUser(
+                sm.getUser().getId(), '', roles, ''
+                )
+
+            # Wrap the user in the acquisition context of the portal
+            tmp_user = tmp_user.__of__(portal.acl_users)
+            newSecurityManager(None, tmp_user)
+
+            # Call the function
+            return function(*args, **kwargs)
+
+        except:
+            # If special exception handlers are needed, run them here
+            raise
+    finally:
+        # Restore the old security manager
+        setSecurityManager(sm)
