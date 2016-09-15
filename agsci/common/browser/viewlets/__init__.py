624f75d648823f12c48b74abcfdd8f0778f8241f
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2016-09-15 09:04:08 -0400

    Customized 'show_history' in byline viewlet to only show it for certain types.

--- agsci/common/browser/viewlets/__init__.py
+++ agsci/common/browser/viewlets/__init__.py
@@ -1,4 +1,7 @@
-from plone.app.layout.viewlets import ViewletBase 
+from plone.app.layout.viewlets import ViewletBase
+from plone.app.layout.viewlets.content import DocumentBylineViewlet as _DocumentBylineViewlet
+from agsci.atlas.content import IAtlasProduct,  IArticleDexterityContent, IArticleDexterityContainedContent
+
 from ..views import FolderView
 
 class LocalSearchViewlet(ViewletBase, FolderView):
@@ -8,3 +11,19 @@ class LocalSearchViewlet(ViewletBase, FolderView):
 
     def search_types(self):
         return getattr(self.context, 'search_types', [])
+
+class DocumentBylineViewlet(_DocumentBylineViewlet):
+
+    def show_history(self):
+
+        is_atlas_content = False
+
+        for i in [IAtlasProduct,  IArticleDexterityContent, IArticleDexterityContainedContent]:
+            if i.providedBy(self.context):
+                is_atlas_content = True
+                break
+
+        if is_atlas_content:
+            return super(DocumentBylineViewlet, self).show_history()
+
+        return False
